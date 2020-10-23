b4706e8a3bd3187b4179c8567f0687b51c0aa238
filmayui <zhongchuyangjie786@gmail.com>
2020-10-23 12:27:06 +0900

    商品購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,19 +3,22 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    unless current_user.id != @item.user.id
+    unless current_user.id != @item.user.id && nil == Order.find_by(item_id: @item.id)
       redirect_to root_path
     end
     @user_order = UserOrder.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @user_order = UserOrder.new(order_params)   
     if @user_order.valid?
+
+      pay_item(@item)
+
       @user_order.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render action: :index
     end
   end
@@ -23,6 +26,15 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:user_order).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:user_order).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item(item)
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: item.price,  
+      card: order_params[:token],    
+      currency: 'jpy'             
+    )
   end
 end
